fa4960e8582a9cdf43bd3266cae5d618931576c5
Bartlomiej Lubiatowski <bartlomiej.lubiatowski@gmail.com>
2015-04-16 19:50:46 +0200

    Decorator pattern examples

---
+++ ConsoleApplication2/Decorator/DebitAccount.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2.Decorator
+{
+    public class DebitAccount : Account
+    {
+        private decimal _allowedDebit;
+
+        public DebitAccount(int clientId, int cardNumber, decimal allowedDebit)
+            : base("debetowe", clientId, cardNumber)
+        {
+            this._allowedDebit = allowedDebit;
+        }
+
+        public override bool isWithdrawalPossible(decimal amount)
+        {
+            if (base.Balance + this.AllowedDebit > amount)
+                return true;
+            else
+                return true;
+        }
+
+        public override void withdraw(decimal amount)
+        {
+            base.withdraw(amount);
+        }
+
+        public decimal AllowedDebit
+        {
+            get { return this._allowedDebit; }
+        }
+
+        public override decimal Balance
+        {
+            get
+            {
+                return base.Balance;
+            }
+        }
+
+        public virtual decimal OwedMoney
+        {
+            get
+            {
+                if (base.Balance > 0) return 0;
+                else return base.Balance;
+            }
+        }
+    }
+}
---
+++ ConsoleApplication2/Decorator/DebitAccountWithInterest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2.Decorator
+{
+    public class DebitAccountWithInterest : DebitAccount
+    {
+        private decimal _interestRate;
+
+        public DebitAccountWithInterest(int clientId, int cardNumber, 
+            decimal allowedDebit, decimal interestRate) 
+            : base(clientId, cardNumber, interestRate)
+        {
+            this._interestRate = interestRate;
+        }
+
+        public override decimal OwedMoney
+        {
+            get
+            {
+                if (base.OwedMoney < 0)
+                    return OwedMoney * this._interestRate;
+                else 
+                    return 0;
+            }
+        }
+
+        public decimal InterestRate
+        {
+            get { return this._interestRate; }
+        }
+    }
+}
